292e97c093ed0f55511be81c435331d12c44ad41
Morethanatester <benpavey@yahoo.com>
2024-10-24 19:02:46 +0100

    Set STATIC_ROOT for all environments

--- benpavey/settings.py
+++ benpavey/settings.py
@@ -126,13 +126,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Set STATIC_ROOT for all environments
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Define project-level static directories
 STATICFILES_DIRS = [
