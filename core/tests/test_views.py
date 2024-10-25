31ac639a70b89d74bbdda5c574026a2e9d3a91d7
Morethanatester <benpavey@yahoo.com>
2024-10-25 16:28:21 +0100

    Test failure test

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -15,5 +15,5 @@ class ViewTests(TestCase):
     # Test that the about view returns a 200 status code and uses the correct template
     def test_about_view(self):
         response = self.client.get(reverse('about'))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 201) # intentional fail
         self.assertTemplateUsed(response, 'core/about.html')
